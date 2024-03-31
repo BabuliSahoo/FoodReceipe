bcfd7dae2de0a9a70cbd17da79bebedec5fb4b46
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-04-01 02:45:42 +0530

    Navigation options set and color properties set

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,75 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  ImageBackground,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
-function MealItem({ title }) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
+  console.log(imageUrl);
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable  style={({ pressed }) =>
+          pressed ? styles.buttonPressed : null
+        }
+        android_ripple={{ color: "#ccc" }} >
+        <View style={styles.innerContainer}>
+          <ImageBackground style={styles.image} source={{ uri: imageUrl }}>
+            <Text style={styles.title}>{title}</Text>
+          </ImageBackground>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailsItem}>{duration}</Text>
+          <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailsItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible", //or android
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    backgroundColor: "white",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+  },
+  detailsItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
